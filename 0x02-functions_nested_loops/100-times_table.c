b5d0f83b53bda7d3b98f46887dfc141f6a899fc2
Oscar-Tangajei <oscartangajei9@mail.com>
2022-03-16 18:42:53 +0000

    push

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,68 +1,47 @@
 #include "main.h"
 /**
- * print_nums - prints out 3, 2, and 1 digit numbers with spaces
- * @num: this is the multiplied number
- * Return: void just pring to console.
- */
-void print_nums(int num)
-{
-int x, y, z;
-
-if (num > 99)
-{
-x = num / 100 + '0';
-y = num / 10 % 10  + '0';
-z = num % 10 + '0';
-_putchar(' ');
-_putchar(x);
-_putchar(y);
-_putchar(z);
-}
-else if (num > 9)
-{
-x = num / 10 + '0';
-y = num % 10 + '0';
-_putchar(' ');
-_putchar(' ');
-_putchar(x);
-_putchar(y);
-}
-else
-{
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-_putchar(num + '0');
-}
-}
-/**
- * print_times_table - print the times table depending on input
- * @n: input that will dicate size of times table.
- *
- * print_nums
- * Return: void, print out to console.
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  */
 void print_times_table(int n)
 {
-int i, j, num;
+	int i, j, k;
 
-if (n >= 0 && n < 16)
-{
-for (i = 0; i <= n; ++i)
-{
-for (j = 0; j <= n; ++j)
-{
-num = i * j;
-if (j == 0)
-_putchar('0');
-else
-print_nums(num);
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
 
-if (j < n)
-_putchar(',');
-else
-_putchar('\n');
-}
-}
-}
-}
